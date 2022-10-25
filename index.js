2dbf4d2ee124cf0129aa622e36b3568fac34ec6b
sajthewizard <rsaj53@gmail.com>
2022-10-25 13:50:55 +0200

    cors done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const data = require('./data/courses.json')
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('LEraning api is running')
